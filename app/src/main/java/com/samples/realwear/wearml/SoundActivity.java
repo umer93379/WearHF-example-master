74536ee4be3226197b81cd890fd3fa72861f2343
Umer Farooq <umer93379@gmail.com>
2018-04-26 16:07:11 +0500

    UMER commit-1

---
+++ app/src/main/java/com/samples/realwear/wearml/SoundActivity.java
@@ -0,0 +1,13 @@
+package com.samples.realwear.wearml;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SoundActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sound);
+    }
+}
